7a4d332f486d2aa1521889f35ad91898c0509617
Tashfia Jannath <76718979+TashfiaJannath@users.noreply.github.com>
2021-05-24 14:16:08 +0800

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,31 @@
+public class Student {
+
+    private String name;
+    private String roll;
+    private String dept;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRoll() {
+        return roll;
+    }
+
+    public void setRoll(String roll) {
+        this.roll = roll;
+    }
+
+    public String getDept() {
+        return dept;
+    }
+
+    public void setDept(String dept) {
+        this.dept = dept;
+    }
+
+}
